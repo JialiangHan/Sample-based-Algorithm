89b8cfba138eba85f78577070fc31c09c166f025
JIaliang Han <hanjiali@umich.edu>
2021-03-06 14:09:00 +0800

    Update RRT_connect.py

--- RRT_connect.py
+++ RRT_connect.py
@@ -3,6 +3,8 @@ import random
 import math
 import copy
 import operator
+import matplotlib.animation as animation
+import numpy as np
 
 
 class Node(object):
@@ -21,49 +23,65 @@ class RRT_connect(object):
     Class for RRT_connect Planning
     """
 
-    def __init__(self, start, goal, obstacle_list, rand_area):
+    def __init__(self, initial, goal, obstacle_list, rand_area):
         """
         Setting Parameter
-
         start:Start Position [x,y]
         goal:Goal Position [x,y]
         obstacleList:obstacle Positions [[x,y,size],...]
         randArea:random sampling Area [min,max]
 
         """
-        self.start = Node(start[0], start[1])
-        self.end = Node(goal[0], goal[1])
+        self.initial = Node(initial[0], initial[1])
+        self.goal = Node(goal[0], goal[1])
         self.min_rand = rand_area[0]
         self.max_rand = rand_area[1]
-        self.expandDis = 0.5  # velocity
+        self.step = 0.5  # velocity
         self.goalSampleRate = 0.5  # 选择终点的概率
         self.maxIter = 5000
         self.obstacleList = obstacle_list
-        self.nodeLists = [self.start]
-        self.nodeListe = [self.end]
+        self.RRT_a = [self.initial]
+        self.RRT_b = [self.goal]
 
     def random_samping(self):
         if random.random() > self.goalSampleRate:
             qrand = self.random_node()
         else:
-            qrand = [self.end.x, self.end.y]
+            qrand = self.goal
         return qrand
 
-    def extend(self,node_list,qrand):
-        qnear=self.nearset_neighbor(node_list,qrand)
-        if self.
+    def connect(self, node_list, qrand):
+        while True:
+            qnew, S = self.extend(node_list, qrand)
+            if S != "Advanced":
+                break
+        return S
 
-    def new_config(self,qnear,qrand):
-        theta = math.atan2(qrand[1] - qnear.y, qrand[0] - qnear.x)
+    def extend(self, node_list, qrand):
+        qnear = self.nearset_neighbor(node_list, qrand)
+        qnew = self.new_config(node_list, qnear, qrand)
+        if qnew is not None:
+            if self.Euclidean_distance(qnew, qrand) < self.step:
+                S = "Reached"
+            else:
+                S = "Advanced"
+        else:
+            S = "Trapped"
+        self.draw_graph(qrand)
+        return qnew, S
+
+    def new_config(self, node_list, qnear, qrand):
+        theta = math.atan2(qrand.y - qnear.y, qrand.x - qnear.x)
         qnew = copy.deepcopy(qnear)
-        qnew.x += self.expandDis * math.cos(theta)
-        qnew.y += self.expandDis * math.sin(theta)
+        qnew.x += self.step * math.cos(theta)
+        qnew.y += self.step * math.sin(theta)
         qnew.parent = qnear
+        if self.collision_check(self.obstacleList, qnew):
+            node_list.append(qnew)
+        else:
+            qnew = None
+        return qnew
 
-        if not self.collision_check(self.obstacleList, [qnew.x, qnew.y]):
-            continue
-
-        self.nodeList.append(qnew)
     def random_node(self):
         """
         产生随机节点
@@ -71,60 +89,58 @@ class RRT_connect(object):
        """
         node_x = random.uniform(self.min_rand, self.max_rand)
         node_y = random.uniform(self.min_rand, self.max_rand)
-        node = [node_x, node_y]
+        node = Node(node_x, node_y)
 
         return node
 
-
-    def nearset_neighbor(self,node_list,qrand):
+    def nearset_neighbor(self, node_list, qrand):
         """
         :param node_list:
         :param qrand:
         :return:
         """
-        d_list = [(node.x - qrand[0]) ** 2 + (node.y - qrand[1]) ** 2 for node in node_list]  # euclidean distance
+        d_list = [(node.x - qrand.x) ** 2 + (node.y - qrand.y) ** 2 for node in node_list]  # euclidean distance
         min_index = d_list.index(min(d_list))
-
-        return node_list(min_index)
+        return node_list[min_index]
 
     def Euclidean_distance(self, node1, node2):
-        dx = node1[0] - node2[0]
-        dy = node1[1] - node2[1]
+        dx = node1.x - node2.x
+        dy = node1.y - node2.y
         d = math.sqrt(dx ** 2 + dy ** 2)
         return d
 
-    def distance_line(self, node, start, end):
+    def distance_line(self, node, initial, goal):
         d = 0
-        if start[0] == end[0]:
-            if (node[1] - start[1]) * (node[1] - end[1]) < 0:
-                d = abs(node[0] - start[0])
-        elif start[1] == end[1]:
-            if (node[0] - start[0]) * (node[0] - end[0]) < 0:
-                d = abs(node[1] - start[1])
+        if initial.x == goal.x:
+            if (node.y - initial.y) * (node.y - goal.y) < 0:
+                d = abs(node.x - initial.x)
+        elif initial.y == goal.y:
+            if (node.x - initial.x) * (node.x - goal.x) < 0:
+                d = abs(node.y - initial.y)
         else:
-            k = (start[1] - end[1]) / (start[0] - end[0])
-            b = start[1] - k * start[0]
-            if (-1 / k * node[0] - node[1] + 1 / k * end[0] + end[1]) * (
-                    -1 / k * node[0] - node[1] + 1 / k * start[0] + start[1]) < 0:
-                d = abs((k * node[0] - node[1] + b) / math.sqrt(k ** 2 + 1))
+            k = (initial.y - goal.y) / (initial.x - goal.x)
+            b = initial.y - k * initial.x
+            if (-1 / k * node.x - node.y + 1 / k * goal.x + goal.y) * (
+                    -1 / k * node.x - node.y + 1 / k * initial.x + initial.y) < 0:
+                d = abs((k * node.x - node.y + b) / math.sqrt(k ** 2 + 1))
             else:
-                ds = self.Euclidean_distance(node, start)
-                de = self.Euclidean_distance(node, end)
+                ds = self.Euclidean_distance(node, initial)
+                de = self.Euclidean_distance(node, goal)
                 d = min(ds, de)
         return d
 
-    def collision_check(self, obstacle_list, start, end=None):
+    def collision_check(self, obstacle_list, initial, goal=None):
         a = 1
         # collision check for node
         for (ox, oy, size) in obstacle_list:
-            dx = ox - start[0]
-            dy = oy - start[1]
+            dx = ox - initial.x
+            dy = oy - initial.y
             d = math.sqrt(dx * dx + dy * dy)
             if d <= size:
                 a = 0  # collision
             # collision check for line segment
-            if end is not None:
-                d2 = self.distance_line([ox, oy], start, end)
+            if goal is not None:
+                d2 = self.distance_line(Node(ox, oy), initial, goal)
                 if d2 <= size:
                     a = 0
         return a  # safe
@@ -138,45 +154,40 @@ class RRT_connect(object):
 
         while True:
             # Random Sampling
-            qrand=self.random_samping()
-
+            qrand = self.random_samping()
             # extend tree
-            self.extend(qrand)
+            qnew, S = self.extend(self.RRT_a, qrand)
+            if S != "Trapped":
+                if self.connect(self.RRT_b, qnew) == "Reached":
+                    path_goal = []
+                    node = self.RRT_b[-1]
+                    while node.parent is not None:
+                        path_goal.append([node.x, node.y])
+                        node = node.parent
+                    path_goal.append([self.goal.x, self.goal.y])
+                    path_initial = []
+                    node = self.RRT_a[-1]
+                    while node.parent is not None:
+                        path_initial.append([node.x, node.y])
+                        node = node.parent
+                    path_initial.append([self.initial.x, self.initial.y])
+                    path_initial.reverse()
+                    path = path_initial + path_goal
+                    return path
+            self.swap()
 
-            # expand tree
-            nearest_node = self.[min_index]
-
-            # 返回弧度制
-
-
-            # check goal
-            dx = qnew.x - self.end.x
-            dy = qnew.y - self.end.y
-            d = math.sqrt(dx * dx + dy * dy)
-            if d <= self.expandDis:
-                print("Goal!!")
-                break
-
-            if True:
-                self.draw_graph(qrand)
-
-        path = [[self.end.x, self.end.y]]
-        last_index = len(self.nodeList) - 1
-        while self.nodeList[last_index].parent is not None:
-            node = self.nodeList[last_index]
-            path.append([node.x, node.y])
-            last_index = node.parent
-        path.append([self.start.x, self.start.y])
-
-        return path
+    def swap(self):
+        a = self.RRT_a
+        self.RRT_a = self.RRT_b
+        self.RRT_b = a
 
     def short_cut(self, path):
         P = path[0]
         new_path = [P]
         i = 1
-        while not operator.eq(path[i], [self.start.x, self.start.y]):
+        while not operator.eq(path[i], [self.initial.x, self.initial.y]):
             # while [P,path[i+1]] collision free and i+1<len(path):
-            while self.collision_check(self.obstacleList, P, path[i + 1]):
+            while self.collision_check(self.obstacleList, Node(P[0], P[1]), Node(path[i + 1][0], path[i + 1][1])):
                 i = i + 1
                 if i + 1 > len(path) - 1:
                     break
@@ -193,17 +204,21 @@ class RRT_connect(object):
         """
         plt.clf()  # 清除上次画的图
         if qrand is not None:
-            plt.plot(qrand[0], qrand[1], "^g")
-        for node in self.nodeList:
+            plt.plot(qrand.x, qrand.y, "^g")
+        for node in self.RRT_a:
             if node.parent is not None:
-                plt.plot([node.x, self.nodeList[node.parent].x], [
-                    node.y, self.nodeList[node.parent].y], "-g")
+                plt.plot([node.x, node.parent.x], [
+                    node.y, node.parent.y], "-g")
+        for node in self.RRT_b:
+            if node.parent is not None:
+                plt.plot([node.x, node.parent.x], [
+                    node.y, node.parent.y], "-g")
 
         for (ox, oy, size) in self.obstacleList:
             plt.plot(ox, oy, marker='o', color='black', ms=44 * size)
 
-        plt.plot(self.start.x, self.start.y, "^r")
-        plt.plot(self.end.x, self.end.y, "^b")
+        plt.plot(self.initial.x, self.initial.y, "^r")
+        plt.plot(self.goal.x, self.goal.y, "^b")
         plt.axis('equal')
         plt.axis([self.min_rand, self.max_rand, self.min_rand, self.max_rand])
         plt.grid(True)
@@ -216,16 +231,20 @@ class RRT_connect(object):
         """
         plt.clf()  # 清除上次画的图
 
-        for node in self.nodeList:
+        for node in self.RRT_a:
             if node.parent is not None:
-                plt.plot([node.x, self.nodeList[node.parent].x], [
-                    node.y, self.nodeList[node.parent].y], "-g")
+                plt.plot([node.x, node.parent.x], [
+                    node.y, node.parent.y], "-g")
+        for node in self.RRT_b:
+            if node.parent is not None:
+                plt.plot([node.x, node.parent.x], [
+                    node.y, node.parent.y], "-r")
 
         for (ox, oy, size) in self.obstacleList:
             plt.plot(ox, oy, marker="o", color='black', ms=44 * size)
 
-        plt.plot(self.start.x, self.start.y, "^r")
-        plt.plot(self.end.x, self.end.y, "^b")
+        plt.plot(self.initial.x, self.initial.y, "^r")
+        plt.plot(self.goal.x, self.goal.y, "^b")
         plt.axis('equal')
         plt.axis([self.min_rand, self.max_rand, self.min_rand, self.max_rand])
 
@@ -238,20 +257,20 @@ class RRT_connect(object):
 
 def main():
     print("start RRT_connect path planning")
-    start = [0, 0]
+    initial = [0, 0]
     goal = [9, 9]
     n = 10  # number of obstacle
     obstacle_list = []
     for i in range(n):
         # random.seed(i)
-        x = random.uniform(start[0], goal[0])
-        y = random.uniform(start[1], goal[1])
+        x = random.uniform(initial[0], goal[0])
+        y = random.uniform(initial[1], goal[1])
         radius = random.uniform(0.5, 1)
         # x, y, radius = 4, 4, 2
         obstacle_list.append((x, y, radius))
 
     # Set Initial parameters
-    rrt = RRT_connect(start, goal, rand_area=[-2, 10], obstacle_list=obstacle_list)
+    rrt = RRT_connect(initial, goal, rand_area=[-2, 10], obstacle_list=obstacle_list)
     path = rrt.planning()
     # print(path)
     new_path = rrt.short_cut(path)
